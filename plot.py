7ede9babcd21e5439ed3db091aee61bca5e66b90
Andrii Natochii <natochii@bnl.gov>
2024-11-15 17:18:42 -0500

    Printout

--- plot.py
+++ plot.py
@@ -130,7 +130,8 @@ def adjust_s(df):
 	return dff
 
 # Draw magnets
-def plot_magnets(data,title,signx,signy):
+def plot_magnets(data,title,signx,signy,signz):
+	print(f"\n\n--> {title}")
 	fig, ax = plt.subplots(figsize=(8, 8))
 	ax.set_title(title)
 	x_ler = signx*np.array(ler_data['Gx'])
@@ -145,9 +146,11 @@ def plot_magnets(data,title,signx,signy):
 		if (row.TypeName == 'BEND') & (row.L > 0.0) & (abs(row.S) < 50.0) & (abs(row.Angle) > 0.0):
 			x1 = signx*row.Gx
 			y1 = signy*row.Gy
+			z1 = signz*row.Gz
 			x0 = signx*data.iloc[row.Index+1].Gx
 			y0 = signy*data.iloc[row.Index+1].Gy
-			w = 0.5
+			z0 = signz*data.iloc[row.Index+1].Gz
+			w = 0.5 # [m]
 
 			theta = np.arctan((y1 - y0) / (x1 - x0))
 			xc = x0 + (w/2.) * np.sin(theta)
@@ -168,6 +171,8 @@ def plot_magnets(data,title,signx,signy):
 			plt.plot([x0, x1],[y0, y1],'-');
 			plt.plot([x0],[y0],marker='o',color='r');
 			plt.plot([x1],[y1],marker='s',color='b');
+
+			print(f"  {row.NAME} : length = {row.L:.6e} [m]; angle = {row.Angle:.6e} [rad]; K0 = {row.K0:.6e}; rot = {row.Rot:.6e} [rad]; (Gx;Gy;Gz)[0] = ({x0:.6e};{y0:.6e};{z0:.6e}) [m]; (Gx;Gy;Gz)[1] = ({x1:.6e};{y1:.6e};{z1:.6e}) [m]")
 
 	ax.grid(True)
 	plt.xlim(-50, 50)
@@ -190,8 +195,8 @@ if __name__ == '__main__':
 	plot_twiss(ler_data,'LER')
 	plot_twiss(her_data,'HER')
 
-	plot_magnets(her_data,'HER',-1,-1)
-	plot_magnets(ler_data,'LER',-1,-1)
+	plot_magnets(her_data,'HER',-1,-1,-1)
+	plot_magnets(ler_data,'LER',-1,-1,-1)
 
 	plt.show()
 
