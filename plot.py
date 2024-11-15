efa1ea6433bf1d859e03fce850c405c37b408a60
Andrii Natochii <natochii@bnl.gov>
2024-11-15 16:59:24 -0500

    Draw magnets

--- plot.py
+++ plot.py
@@ -1,62 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Function to read the file and parse the data
-def read_data(filename):
-	data = {
-		'NAME': [], 'S': [], 'Gx': [], 'Gy': [], 'Gz': [], 'OGx': [], 'OGy': [], 'OGz': [],
-		'Ax': [], 'Bx': [], 'Nx': [], 'Ex': [], 'EPx': [], 'Ay': [], 'By': [], 'Ny': [], 'Ey': [], 'EPy': [],
-		'L': [], 'Type': [], 'TypeName': [], 'Angle': [], 'K1': [], 'Rot': [], 'Dx': [], 'Dy': []
-	}
-	with open(filename, 'r') as file:
-		next(file)  # Skip the header line
-		for line in file:
-			# Split the line by spaces and remove extra whitespaces
-			parts = line.split()
-			for index, key in enumerate(data):
-				if key == "NAME" or key == "TypeName":
-					data[key].append(parts[index])
-				else:
-					data[key].append(float(parts[index]))
-	return data
-
-# Rotate coordinates
-def rotate(x_orig,y_orig,rot_ang):
-	x_rot = x_orig * np.cos(rot_ang) - y_orig * np.sin(rot_ang)
-	y_rot = x_orig * np.sin(rot_ang) + y_orig * np.cos(rot_ang)
-	return (x_rot,y_rot)
+import pandas as pd
+import matplotlib.patches as patches
+import matplotlib as mpl
+import math
 
 # Plot Gx vs Gy
-def plot_skb(ler_data,her_data,signx,signy):
-	rot_ang = 0 #np.float64(8.5e-3)
+def plot_rings(ler_data,her_data,signx,signy):
 	plt.figure(figsize=(8, 8))
 
 	ler_coll_indices = [i for i, name in enumerate(ler_data['NAME']) if name.startswith("PMD")]
 	her_coll_indices = [i for i, name in enumerate(her_data['NAME']) if name.startswith("PMD")]
 
-	ler_coll_x_orig = signx*np.array([ler_data['Gx'][i] for i in ler_coll_indices])
-	ler_coll_y_orig = signy*np.array([ler_data['Gy'][i] for i in ler_coll_indices])
-	her_coll_x_orig = signx*np.array([her_data['Gx'][i] for i in her_coll_indices])
-	her_coll_y_orig = signy*np.array([her_data['Gy'][i] for i in her_coll_indices])
+	ler_coll_x = signx*np.array([ler_data['Gx'][i] for i in ler_coll_indices])
+	ler_coll_y = signy*np.array([ler_data['Gy'][i] for i in ler_coll_indices])
+	her_coll_x = signx*np.array([her_data['Gx'][i] for i in her_coll_indices])
+	her_coll_y = signy*np.array([her_data['Gy'][i] for i in her_coll_indices])
 
-	ler_coll_x, ler_coll_y = rotate(ler_coll_x_orig,ler_coll_y_orig,rot_ang)
-	her_coll_x, her_coll_y = rotate(her_coll_x_orig,her_coll_y_orig,rot_ang)
+	x_ler = signx*np.array(ler_data['Gx'])
+	y_ler = signy*np.array(ler_data['Gy'])
 
-	x_orig = signx*np.array(ler_data['Gx'])
-	y_orig = signy*np.array(ler_data['Gy'])
-	x_ler, y_ler = rotate(x_orig,y_orig,rot_ang)
+	x_her = signx*np.array(her_data['Gx'])
+	y_her = signy*np.array(her_data['Gy'])
 
-	x_orig = signx*np.array(her_data['Gx'])
-	y_orig = signy*np.array(her_data['Gy'])
-	x_her, y_her = rotate(x_orig,y_orig,rot_ang)
+	ox_ler = signx*np.array(ler_data['OGx'])
+	oy_ler = signy*np.array(ler_data['OGy'])
 
-	x_orig = signx*np.array(ler_data['OGx'])
-	y_orig = signy*np.array(ler_data['OGy'])
-	ox_ler, oy_ler = rotate(x_orig,y_orig,rot_ang)
-
-	x_orig = signx*np.array(her_data['OGx'])
-	y_orig = signy*np.array(her_data['OGy'])
-	ox_her, oy_her = rotate(x_orig,y_orig,rot_ang)
+	ox_her = signx*np.array(her_data['OGx'])
+	oy_her = signy*np.array(her_data['OGy'])
 
 	plt.plot(x_ler, y_ler, linestyle='-', color='r', label="LER")
 	plt.plot(x_her, y_her, linestyle='-', color='b', label="HER")
@@ -72,13 +43,155 @@ def plot_skb(ler_data,her_data,signx,signy):
 	plt.ylabel('Y')
 	plt.grid(True)
 	plt.legend()
-	plt.show()
+
+# Plot Twiss parameters
+def plot_twiss(data,ring):
+	s_twiss = np.array(data['S'])
+	ax_twiss = np.array(data['Ax'])
+	ay_twiss = np.array(data['Ay'])
+	bx_twiss = np.array(data['Bx'])
+	by_twiss = np.array(data['By'])
+	ex_twiss = np.array(data['Ex'])
+	ey_twiss = np.array(data['Ey'])
+	epx_twiss = np.array(data['EPx'])
+	epy_twiss = np.array(data['EPy'])
+	dx_twiss = np.array(data['Dx'])
+	dy_twiss = np.array(data['Dy'])
+	l_twiss = np.array(data['L'])
+
+	# Create a figure and two subplots
+	fig, ax = plt.subplots(2, 3, figsize=(16, 8))
+
+	# alpha
+	ax[0, 0].plot(s_twiss, ax_twiss, color='blue', linestyle='-', label="X")
+	ax[0, 0].plot(s_twiss, ay_twiss, color='red', linestyle='--', label="Y")
+	ax[0, 0].set_title(f"Twiss: {ring}")
+	ax[0, 0].set_xlabel(r"S [m]")
+	ax[0, 0].set_ylabel(r"$\alpha$")
+	ax[0, 0].legend()
+	ax[0, 0].grid(True)
+
+	# beta
+	ax[1, 0].plot(s_twiss, bx_twiss, color='blue', linestyle='-', label="X")
+	ax[1, 0].plot(s_twiss, by_twiss, color='red', linestyle='--', label="Y")
+	ax[1, 0].set_title(f"Twiss: {ring}")
+	ax[1, 0].set_xlabel(r"S [m]")
+	ax[1, 0].set_ylabel(r"$\beta$ [m]")
+	ax[1, 0].legend()
+	ax[1, 0].grid(True)
+
+	# eta
+	ax[0, 1].plot(s_twiss, ex_twiss, color='blue', linestyle='-', label="X")
+	ax[0, 1].plot(s_twiss, ey_twiss, color='red', linestyle='--', label="Y")
+	ax[0, 1].set_title(f"Twiss: {ring}")
+	ax[0, 1].set_xlabel(r"S [m]")
+	ax[0, 1].set_ylabel(r"$\eta$ [m]")
+	ax[0, 1].legend()
+	ax[0, 1].grid(True)
+
+	# eta'
+	ax[1, 1].plot(s_twiss, epx_twiss, color='blue', linestyle='-', label="X")
+	ax[1, 1].plot(s_twiss, epy_twiss, color='red', linestyle='--', label="Y")
+	ax[1, 1].set_title(f"Twiss: {ring}")
+	ax[1, 1].set_xlabel(r"S [m]")
+	ax[1, 1].set_ylabel(r"$\eta'$")
+	ax[1, 1].legend()
+	ax[1, 1].grid(True)
+
+	# d
+	ax[0, 2].plot(s_twiss, dx_twiss * 1e3, color='blue', linestyle='-', label="X")
+	ax[0, 2].plot(s_twiss, dy_twiss * 1e3, color='red', linestyle='--', label="Y")
+	ax[0, 2].set_title(f"Twiss: {ring}")
+	ax[0, 2].set_xlabel(r"S [m]")
+	ax[0, 2].set_ylabel(r"$D$ [mm]")
+	ax[0, 2].legend()
+	ax[0, 2].grid(True)
+
+	# d
+	ax[1, 2].plot(s_twiss, l_twiss, color='green', linestyle='-')
+	ax[1, 2].set_title(f"Twiss: {ring}")
+	ax[1, 2].set_xlabel(r"S [m]")
+	ax[1, 2].set_ylabel(r"$L$ [m]")
+	ax[1, 2].grid(True)
+
+	# Adjust layout to prevent overlap
+	plt.tight_layout()
+
+# Adjust S coordinate [0;ring_length] -> [-ring_length/2;+ring_length/2]
+def adjust_s(df):
+	ring_length = df['S'].max() - df['S'].min()
+	print(f"--> Ring length = {ring_length:.3f} [m]")
+	# Replace S values greater than the half of the ring length
+	df.loc[df['S'] > ring_length/2., 'S'] = df['S'] - ring_length
+	# Sort the DataFrame by S in ascending order
+	df_sorted = df.sort_values(by='S')
+	# Reset the index if desired (optional)
+	dff = df_sorted.reset_index(drop=True)
+	return dff
+
+# Draw magnets
+def plot_magnets(data,title,signx,signy):
+	fig, ax = plt.subplots(figsize=(8, 8))
+	ax.set_title(title)
+	x_ler = signx*np.array(ler_data['Gx'])
+	y_ler = signy*np.array(ler_data['Gy'])
+	x_her = signx*np.array(her_data['Gx'])
+	y_her = signy*np.array(her_data['Gy'])
+
+	plt.plot(x_ler, y_ler, linestyle='-.', color='r', label="LER")
+	plt.plot(x_her, y_her, linestyle='-.', color='b', label="HER")
+
+	for row in data.itertuples():
+		if (row.TypeName == 'BEND') & (row.L > 0.0) & (abs(row.S) < 50.0) & (abs(row.Angle) > 0.0):
+			x1 = signx*row.Gx
+			y1 = signy*row.Gy
+			x0 = signx*data.iloc[row.Index+1].Gx
+			y0 = signy*data.iloc[row.Index+1].Gy
+			w = 0.5
+
+			theta = np.arctan((y1 - y0) / (x1 - x0))
+			xc = x0 + (w/2.) * np.sin(theta)
+			yc = y0 - (w/2.) * np.cos(theta)
+
+			linestyle = '-'
+			edgecolor = 'black'
+			linewidth = 2
+			if abs(row.Rot) > 0:
+				linestyle = '--'
+				edgecolor = 'gray' 
+				linewidth = 1
+
+			rect = patches.Rectangle((xc, yc), row.L, w, angle=(theta*180.0/math.pi), 
+				linewidth=linewidth, linestyle=linestyle, edgecolor=edgecolor, facecolor='none')
+
+			ax.add_patch(rect)
+			plt.plot([x0, x1],[y0, y1],'-');
+			plt.plot([x0],[y0],marker='o',color='r');
+			plt.plot([x1],[y1],marker='s',color='b');
+
+	ax.grid(True)
+	plt.xlim(-50, 50)
+	plt.ylim(-2, 2)
 
 # Main function to run the script
 if __name__ == '__main__':
 	her_filename = 'sher_2021-06-16_1.0A_1x60_nb_1576_cw_40.survey'
 	ler_filename = 'sler_2021-06-16_1.2A_1x80_nb_1576_cw_80.survey'
-	her_data = read_data(her_filename)
-	ler_data = read_data(ler_filename)
-	plot_skb(ler_data,her_data,-1,-1)
+
+	her_data = adjust_s(pd.read_csv(her_filename, sep='\s+'))
+	ler_data = adjust_s(pd.read_csv(ler_filename, sep='\s+'))
+
+	print("\n====  HER ====")
+	print(her_data.head())
+	print("\n====  LER ====")
+	print(ler_data.head())
+	plot_rings(ler_data,her_data,-1,-1)
+
+	plot_twiss(ler_data,'LER')
+	plot_twiss(her_data,'HER')
+
+	plot_magnets(her_data,'HER',-1,-1)
+	plot_magnets(ler_data,'LER',-1,-1)
+
+	plt.show()
 
